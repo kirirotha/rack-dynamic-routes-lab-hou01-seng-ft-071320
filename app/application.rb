362d263a9b2cedd2fef83b917379f6b2dc7a8324
Kiri Um <kirirotha@gmail.com>
2020-08-03 20:55:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
 
      this_thing = req.params["item"]
 
-    if req.path.match(/items/this_thing.name})
+    if req.path.match(/items/this_thing.name)
         resp.write "#{this_thing.price}\n"
         resp.status 200
     else
