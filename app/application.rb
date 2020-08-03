8b25001b4986a913ced80f5c032130754ba84997
Kiri Um <kirirotha@gmail.com>
2020-08-03 21:29:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{ |m| m.name = item_name}
-      binding.pry
+      #binding.pry
 
       if @@items.include?(item)
         resp.write item.price
