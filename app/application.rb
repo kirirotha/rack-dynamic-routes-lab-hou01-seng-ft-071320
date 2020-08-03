5295a091d7190ac4eab73b49fab0a8e27449b00b
Kiri Um <kirirotha@gmail.com>
2020-08-03 21:04:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{ |m| m.name = item_name}
-      resp.write item.price 
+      resp.write item.price
       resp.status = 200
     else
       resp.write "Path not found"
