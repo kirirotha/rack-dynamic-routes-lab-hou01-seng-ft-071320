b696f444e130060334ccfbdc83323d278e93f0cf
Kiri Um <kirirotha@gmail.com>
2020-08-03 21:20:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,8 +9,13 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{ |m| m.name = item_name}
+      if item == nil
+        resp.status = 400
+        resp.write "Item not found"
+      else
       resp.write item.price
       resp.status = 200
+      end
     else
       resp.write "Route not found"
       resp.status = 404
