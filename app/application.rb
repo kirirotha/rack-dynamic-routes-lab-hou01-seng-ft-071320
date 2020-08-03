57d8be9b67fbf64354a719e869f1d59bccb35168
Kiri Um <kirirotha@gmail.com>
2020-08-03 21:08:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-
+    binding.pry
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{ |m| m.name = item_name}
