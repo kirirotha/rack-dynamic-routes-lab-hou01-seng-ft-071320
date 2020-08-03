909711637ead0d3fa65c7554717638b69a69d2bb
Kiri Um <kirirotha@gmail.com>
2020-08-03 20:51:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,20 @@
+require_relative './item.rb'
+
 class application
 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
+    if req.path.match(/items/)
+          @@items.each do |item|
+            resp.write "#{item}\n"
+          end
+        resp.status 200
+    else
+      resp.write "Path not found"
+      resp.status = 404
+  end
 
 
 end
